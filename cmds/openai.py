079b59e074f1c4e8c6331056299726f8404f004d
Yurei <102242656+SKer001@users.noreply.github.com>
2023-09-15 13:59:54 +0800

    Update openai.py

--- cmds/openai.py
+++ cmds/openai.py
@@ -5,3 +5,8 @@ from discord import app_commands
 import random 
 import json 
 from core.Def import reback
+
+class CardBettleSys(Cog_Extension): 
+    pass 
+async def setup(bot): 
+    await bot.add_cog(CardBettleSys(bot))
