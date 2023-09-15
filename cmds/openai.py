dcffdef6877f40bc0dedafc197fd61fa869c018d
Yurei <102242656+SKer001@users.noreply.github.com>
2023-09-15 11:42:31 +0800

    Update openai.py

--- cmds/openai.py
+++ cmds/openai.py
@@ -1,1 +1,7 @@
-
+import discord 
+from core.classes import Cog_Extension 
+from discord.ext import commands 
+from discord import app_commands 
+import random 
+import json 
+from core.Def import reback
