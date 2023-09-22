8a4082281e42f7461f6521c8206ed106a7b7c08a
SKer001 <102242656+SKer001@users.noreply.github.com>
2023-09-22 14:24:57 +0000

    ch

--- core/Def.py
+++ core/Def.py
@@ -19,55 +19,53 @@ path = "log/" + str(
 
 
 class custom_bot_command():
+  ################################################################################################################################
+  def create_monster_card(name: str, ATK: int, DEF: int, MATK: int, MDEF: int,
+                          HP: int, AGI: int, CON: str, SP: int):
+    with open(f"cards/monster.json", "r", encoding="utf-8") as file:
+      filecard = list(json.load(file))
+    card_dic_basic = {
+        "name": name,
+        "ATK": ATK,
+        "DEF": DEF,
+        "MATK": MATK,
+        "MDEF": MDEF,
+        "HP": HP,
+        "AGI": AGI,
+        "CON": CON,
+        "SP": SP,
+        "description": ""
+    }
+    filecard.append(card_dic_basic)
+    with open(f"cards/monster.json", "w", encoding="utf-8") as CardFile:
+      json.dump(filecard, CardFile, indent=4, ensure_ascii=False)
 ################################################################################################################################
-    def create_monster_card(name: str, ATK: int, DEF: int, MATK: int, MDEF: int,HP: int, AGI: int, CON: str, SP: int):
-        with open(f"cards/monster.json", "r", encoding="utf-8") as file:
-            filecard = list(json.load(file))
-        card_dic_basic = {
-            "name": name,
-            "ATK": ATK,
-            "DEF": DEF,
-            "MATK": MATK,
-            "MDEF": MDEF,
-            "HP": HP,
-            "AGI": AGI,
-            "CON": CON,
-            "SP": SP,
-            "description": ""
-        }
-        filecard.append(card_dic_basic)
-        with open(f"cards/monster.json", "w", encoding="utf-8") as CardFile:
-            json.dump(filecard, CardFile, indent=4, ensure_ascii=False)
+
+  def create_trap_card(name: str, SP: int):
+    with open(f"cards/traps.json", "r", encoding="utf-8") as file:
+      filecard = list(json.load(file))
+    card_dic_basic = {"skill-name": name, "SP": SP, "skill-description": ""}
+    filecard.append(card_dic_basic)
+    with open(f"cards/traps.json", "w", encoding="utf-8") as CardFile:
+      json.dump(filecard, CardFile, indent=4, ensure_ascii=False)
 ################################################################################################################################
-    def create_trap_card(name:str,SP: int):
-        with open(f"cards/traps.json", "r", encoding="utf-8") as file:
-            filecard = list(json.load(file))
-        card_dic_basic = {
-            "skill-name": name,
-            "SP": SP,
-            "skill-description": ""
-        }
-        filecard.append(card_dic_basic)
-        with open(f"cards/traps.json", "w", encoding="utf-8") as CardFile:
-            json.dump(filecard, CardFile, indent=4, ensure_ascii=False)
+
+  def create_magic_card(name: str, SP: int):
+    with open(f"cards/magics.json", "r", encoding="utf-8") as file:
+      filecard = list(json.load(file))
+    card_dic_basic = {"skill-name": name, "SP": SP, "skill-description": ""}
+    filecard.append(card_dic_basic)
+    with open(f"cards/magics.json", "w", encoding="utf-8") as CardFile:
+      json.dump(filecard, CardFile, indent=4, ensure_ascii=False)
+
+
 ################################################################################################################################
-    def create_magic_card(name:str,SP: int):
-        with open(f"cards/magics.json", "r", encoding="utf-8") as file:
-            filecard = list(json.load(file))
-        card_dic_basic = {
-            "skill-name": name,
-            "SP": SP,
-            "skill-description": ""
-        }
-        filecard.append(card_dic_basic)
-        with open(f"cards/magics.json", "w", encoding="utf-8") as CardFile:
-            json.dump(filecard, CardFile, indent=4, ensure_ascii=False)
-################################################################################################################################
-    def reback(user_name, user_id, command):
-        print(
-            f"{datetime.datetime.utcnow()} used {command} by {user_name} {user_id}"
-        )
-        with open(path, "a", encoding="utf-8") as LogFile:
-            LogFile.write(
-                f"{datetime.datetime.now()} used {command} by {user_name} {user_id}\n"
-            )
+
+  def reback(user_name, user_id, command):
+    print(
+        f"{datetime.datetime.utcnow()} used {command} by {user_name} {user_id}"
+    )
+    with open(path, "a", encoding="utf-8") as LogFile:
+      LogFile.write(
+          f"{datetime.datetime.now()} used {command} by {user_name} {user_id}\n"
+      )
